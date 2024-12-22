ac10ecc00ce154444664405f8322ac1b7ee99899
Abdoulaye CISSOKHO <ab.cissokho87@gmail.com>
2024-12-22 01:24:37 +0100

    fix: add users.controlers.js and users.routes.js

---
+++ api/src/controllers/users.controllers.js
@@ -0,0 +1,23 @@
+const { StatusCodes } = require("http-status-codes");
+const { findUserByEmail, createUser } = require("../queries/user.querie");
+
+exports.signup = async (req, res) => {
+    try {
+        const body = req.body;
+        const email = await findUserByEmail(body.local.email);
+        if (email) {
+            res.json({
+                status: StatusCodes.BAD_REQUEST,
+                message: "L'email existe, Veuillez vous connecter",
+            });
+        } else {
+            const userSaved = await createUser(body);
+            res.json({
+                status: StatusCodes.CREATED,
+                message: "User enregistré avec succès",
+            });
+        }
+    } catch (error) {
+        throw error;
+    }
+};
