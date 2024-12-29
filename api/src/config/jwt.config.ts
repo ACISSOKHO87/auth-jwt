f9d56f00a7e4f013afb64684573b3e43c0ff6814
Abdoulaye CISSOKHO <ab.cissokho87@gmail.com>
2024-12-29 04:12:58 +0100

    add jwst.config.ts and users (controllers, queries, interfaces) files

---
+++ api/src/config/jwt.config.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { IUser } from "../interface/user.interface";
+
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
+
+export const createJwtToken = (user: IUser) => {
+    const jwtToken = jwt.sign(
+        {
+            sub: user._id!.toString(),
+            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, // durée de validité 24h
+            algorithm: "RS256",
+        },
+        process.env.SECRET
+    );
+    return jwtToken;
+};
+
+exports.extractUserFromToken = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { token } = req.cookies;
+    jwt.verify(token, process.env.SECRET, (err: any, decode: any) => {
+        if (err) {
+            res.clearCookie("token");
+            res.json({
+                status: StatusCodes.UNAUTHORIZED,
+                message: "Token invalide",
+            });
+        } else {
+            req.body.userId = decode.sub;
+            next();
+        }
+    });
+};
--- api/src/interface/user.interface.ts
+++ api/src/interface/user.interface.ts
@@ -1,13 +1,16 @@
-import { Document } from "mongoose";
-
 export interface IUserLocal {
     email: string;
     password: string;
 }
 
-export interface IUser extends Document {
+export interface IUser {
+    _id?: string;
     name: string;
     local: IUserLocal;
+    comparePassword: (
+        password: string,
+        hashedPassword: string
+    ) => Promise<boolean>;
 }
 
 export interface UserForm {
